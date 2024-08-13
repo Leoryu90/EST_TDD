2fde3f81488ab203c6c7e3eed3de084c819b8086
Seokho ryu <thetein@gmail.com>
2024-08-13 09:48:58 +0900

    Initial commit

---
+++ src/main/java/com/example/est_tdd/EstTddApplication.java
@@ -0,0 +1,13 @@
+package com.example.est_tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstTddApplication.class, args);
+    }
+
+}
